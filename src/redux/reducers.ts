cc9ef80d9d27cfaff979d5f81ca2f8e9b3278e2e
razvan.tomegea <razvan.tomegea@gmail.com>
2024-06-11 19:23:21 +0300

    Removes account slice

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,16 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import reduxSessionStorage from 'redux-persist/lib/storage/session';
-import { persistReducer } from 'redux-persist';
-import { accountReducer } from './slices';
 import { RootApi } from './rootApi';
 
-const accountPersisted = {
-  key: 'account',
-  storage: reduxSessionStorage,
-  blacklist: ['tokenLogin', 'token']
-};
-
 export const rootReducer = combineReducers({
-  [RootApi.reducerPath]: RootApi.reducer,
-  account: persistReducer(accountPersisted, accountReducer)
+  [RootApi.reducerPath]: RootApi.reducer
 });
