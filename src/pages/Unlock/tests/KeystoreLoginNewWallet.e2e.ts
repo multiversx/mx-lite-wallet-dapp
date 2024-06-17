2c775c9f776d18c7f5b23c0365f3c9abb0b081a8
razvan.tomegea <razvan.tomegea@gmail.com>
2024-06-17 18:37:01 +0300

    Fixed KeystoreLoginNewWallet.e2e.ts

--- src/pages/Unlock/tests/KeystoreLoginNewWallet.e2e.ts
+++ src/pages/Unlock/tests/KeystoreLoginNewWallet.e2e.ts
@@ -1,13 +1,11 @@
 import { WALLET_SOURCE_ORIGIN } from '__mocks__/data/constants';
 import {
   emptyWalletAccount,
-  emptyWalletKeystoreAddresses,
   emptyWalletPassword
 } from '__mocks__/data/emptyWallet';
 import { DataTestIdsEnum } from 'localConstants/dataTestIds.enum';
 import {
   changeInputText,
-  expectElementToBeDisabled,
   expectElementToContainText,
   expectToBeChecked,
   getByDataTestId,
@@ -35,56 +33,11 @@ describe('New wallet login with keystore test', () => {
     });
 
     await page.click(getByDataTestId(DataTestIdsEnum.submitButton));
-
     const dataTestId = `check_${emptyWalletAccount.address}`;
-    await page.click(getByDataTestId(dataTestId));
+    await page.waitForSelector(getByDataTestId(dataTestId));
 
     await expectToBeChecked({
       dataTestId,
-      isChecked: true
-    });
-
-    await page.click(getByDataTestId(DataTestIdsEnum.nextBtn));
-
-    // The Confirm button must be disabled when clicking next
-    await expectElementToBeDisabled({
-      dataTestId: DataTestIdsEnum.confirmBtn,
-      isDisabled: true
-    });
-
-    const emptyAddressDataTestId = `check_${emptyWalletKeystoreAddresses[10]}`;
-
-    // The first address in the page must not be selected when clicking next
-    await expectToBeChecked({
-      dataTestId: emptyAddressDataTestId,
-      isChecked: false
-    });
-
-    await page.click(getByDataTestId(DataTestIdsEnum.prevBtn));
-
-    // The initial address is still selected
-    await expectToBeChecked({
-      dataTestId,
-      isChecked: true
-    });
-
-    await expectElementToBeDisabled({
-      dataTestId: DataTestIdsEnum.confirmBtn,
-      isDisabled: false
-    });
-
-    await page.click(getByDataTestId(DataTestIdsEnum.nextBtn));
-
-    await expectToBeChecked({
-      dataTestId: emptyAddressDataTestId,
-      isChecked: false
-    });
-
-    // Select the first address from the second page
-    await page.click(getByDataTestId(emptyAddressDataTestId));
-
-    await expectToBeChecked({
-      dataTestId: emptyAddressDataTestId,
       isChecked: true
     });
 
@@ -92,7 +45,7 @@ describe('New wallet login with keystore test', () => {
 
     await expectElementToContainText({
       dataTestId: DataTestIdsEnum.userAddress,
-      text: `${emptyWalletKeystoreAddresses[10]}${emptyWalletKeystoreAddresses[10]}`
+      text: `${emptyWalletAccount.address}`
     });
 
     expect(page.url()).toMatch(`${WALLET_SOURCE_ORIGIN}/dashboard`);
