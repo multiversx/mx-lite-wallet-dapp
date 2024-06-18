76853d8d077ea0ac39608460751c94b9fb5ca69a
Tudor Morar <tudor.morar@multiversx.com>
2024-06-18 17:51:15 +0300

    Add preference for persistance (#19)
    * Work on sign hook
    
    * Some refactoring
    
    * Organize imports
    
    * Sign compiling
    
    * Sign hook working
    
    * Lint
    
    * Refactor working
    
    * Refactor sign hook working
    
    * Add explenations
    
    * Add preference for persistance
    
    * Add prefernce for persistance

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,11 +1,23 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import reduxSessionStorage from 'redux-persist/lib/storage/session';
+import reduxLocalStorage from 'redux-persist/lib/storage';
 import { RootApi } from './rootApi';
 import { hookReducer, accountReducer } from './slices';
 
 export const storageIgnoredSlices = ['hook'];
 
+const accountPersisted = {
+  key: 'account',
+  storage:
+    import.meta.env.VITE_APP_PERSIST === 'localStorage'
+      ? reduxLocalStorage
+      : reduxSessionStorage,
+  blacklist: ['tokenLogin', 'token']
+};
+
 export const rootReducer = combineReducers({
   hook: hookReducer,
-  account: accountReducer,
+  account: persistReducer(accountPersisted, accountReducer),
   [RootApi.reducerPath]: RootApi.reducer
 });
