1f8c970ab648498eb6a071b6410a72582fc3e15d
Tudor Morar <tudor.morar@multiversx.com>
2024-06-25 16:32:41 +0300

    Add sovereign config

--- src/pages/Unlock/hooks/useInitToken.ts
+++ src/pages/Unlock/hooks/useInitToken.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useGetLoginInfo, useLoginService } from 'lib';
+import { useGetLoginInfo, useGetNetworkConfig, useLoginService } from 'lib';
 import { HooksEnum } from 'localConstants';
 import { hookSelector } from 'redux/selectors';
 import { setToken } from 'redux/slices';
@@ -8,7 +8,13 @@ export const useInitToken = () => {
   const dispatch = useDispatch();
   const { type: hook, loginToken } = useSelector(hookSelector);
   const { isLoggedIn } = useGetLoginInfo();
-  const loginService = useLoginService();
+  const {
+    network: { apiAddress }
+  } = useGetNetworkConfig();
+
+  const loginService = useLoginService({
+    apiAddress
+  });
 
   const getInitToken = async () => {
     if (hook === HooksEnum.login) {
