0d5a2fe00bab73c1ad0657e15418dcc0952887d9
gencher <gencher@live.com>
2024-06-27 15:04:27 +0300

    auto deploy netlify

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import { EnvironmentsEnum } from 'types';
+
+export * from './sharedConfig';
+
+export const API_URL = 'https://testnet-api.cyber.network'; // replace here with actual sovereign URL
+export const sampleAuthenticatedDomains = [API_URL];
+export const environment = EnvironmentsEnum.testnet;
