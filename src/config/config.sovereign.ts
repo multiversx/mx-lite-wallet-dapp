c565ef63644104858f51d2354b24c22e935ec5be
Tudor Morar <tudor.morar@multiversx.com>
2024-06-27 13:18:24 +0300

    Add soverign environment (#28)

--- src/config/config.sovereign.ts
+++ src/config/config.sovereign.ts
@@ -1,7 +1,5 @@
-import { EnvironmentsEnum } from 'types';
-
 export * from './sharedConfig';
 
 export const API_URL = 'https://devnet-api.multiversx.com'; // replace here with actual sovereign URL
 export const sampleAuthenticatedDomains = [API_URL];
-export const environment = EnvironmentsEnum.devnet;
+export const environment = 'sovereign';
