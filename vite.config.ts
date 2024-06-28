03927d9261ea663e15d47a5a276ecc27f95a2310
Tudor Morar <tudor.morar@multiversx.com>
2024-06-28 11:18:46 +0300

    Added default token in list

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   server: {
