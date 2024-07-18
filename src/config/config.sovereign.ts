981124df28f6aa6cedf123d4fa61fee0e398acbb
razvan.tomegea <razvan.tomegea@gmail.com>
2024-07-18 15:56:50 +0300

    Revert config.sovereign.ts

--- src/config/config.sovereign.ts
+++ src/config/config.sovereign.ts
@@ -1,5 +1,5 @@
 export * from './sharedConfig';
 
-export const API_URL = 'https://api-sovereign-test.elrond.ro'; // replace here with actual sovereign URL
+export const API_URL = 'https://devnet-api.multiversx.com'; // replace here with actual sovereign URL
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = 'sovereign';
