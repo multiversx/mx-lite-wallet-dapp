89aaeee2dced7f2117ffe38fee643f90a6da8836
Razvan Tomegea <razvan.tomegea@gmail.com>
2025-03-25 17:44:10 +0200

    Fixed build

--- src/components/AxiosInterceptor/AxiosInterceptor.tsx
+++ src/components/AxiosInterceptor/AxiosInterceptor.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useGetAccount } from 'lib';
-import { networkSelector } from 'redux/selectors';
 import {
   handleError,
   useSetNativeAuthInterceptors,
@@ -9,7 +7,6 @@ import {
 } from './helpers';
 
 export const AxiosInterceptor = ({ children }: React.PropsWithChildren) => {
-  const { accessToken: hasAccessToken } = useSelector(networkSelector);
   const { setNativeAuthTokenInterceptors, nativeAuthToken } =
     useSetNativeAuthInterceptors();
   const { address } = useGetAccount();
