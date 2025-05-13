095ad918c046fec64be88bc62bb96508b73a7b43
Tudor Homoranu <tudor.homoranu@multiversx.com>
2025-05-13 11:51:17 +0300

    timeout added

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,9 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:3000',
     ignoreHTTPSErrors: true,
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+    navigationTimeout: 30000, // Timeout for each navigation in milliseconds
+    actionTimeout: 10000
   },
 
   /* Configure projects for major browsers */
