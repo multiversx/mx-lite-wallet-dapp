4959d075a0b9c1b6a0fab4d03b62559b8a929522
Razvan Tomegea <razvan.tomegea@gmail.com>
2025-08-05 20:45:56 +0300

    Update mock wallet address in emptyWallet.ts to reflect new VibeChain format

--- src/__mocks__/data/emptyWallet/emptyWallet.ts
+++ src/__mocks__/data/emptyWallet/emptyWallet.ts
@@ -1,6 +1,6 @@
 export const emptyWalletPassword = 'juxbit-7dusku-miskIs';
 export const emptyWalletAccount = {
-  address: 'vibe1q4teee5c32dw6dk2wg5hgcr356u0vqlewqjlzhk3d3chtzm2mnvsqkpl4w',
+  address: 'vibe1m7x4d8r6kj43082pew8cj5vhqhw3gtk6g99r044d4qxq08jpq6gs8x4xh4',
   balance: '0',
   nonce: 0,
   timestamp: 0,
