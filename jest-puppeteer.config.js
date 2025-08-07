c814c8981846afe23b004d9910cbf5f6417a478d
Razvan Tomegea <razvan.tomegea@gmail.com>
2025-08-07 19:35:16 +0300

    Update Jest Puppeteer configuration to set maxWorkers to 2 for improved performance in headless mode

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -28,7 +28,7 @@ const config = {
   modulePaths: ['<rootDir>/src'],
   bail: 1,
   workerIdleMemoryLimit: '512MB',
-  maxWorkers: isHeadless ? '50%' : 1,
+  maxWorkers: isHeadless ? 2 : 1,
   launch: {
     slowMo: 0,
     headless: isHeadless,
